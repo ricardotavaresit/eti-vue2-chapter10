37a9aad201c1eb69f3d68afb850b69927837cc88
rtavares <ricardotavaresit@gmail.com>
2022-05-30 21:20:00 +0100

    create ListItemsComponent

---
+++ src/components/ListItemsComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    {{ title }}
+  </div>
+
+</template>
+
+<script>
+export default {
+  props:{
+    title:{
+      required: true,
+      type: String,
+      default: ""
+    }
+  },
+}
+
+</script>
+
+
+<style>
+</style>
